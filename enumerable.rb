1cf9fd8d77bf37099dc15f3ea12acb9841bbe4bc
Urchmaney <kingsobino@gmail.com>
2019-08-07 18:05:39 +0000

    Linter :: add spaces and changes

--- enumerable.rb
+++ enumerable.rb
@@ -1,58 +1,58 @@
 module Enumerable
-  def my_each     
+  def my_each
     i = 0
-    while i < self.length
+    while i < length
       yield(self[i])
       i += 1
     end
   end
 
-  def my_each_with_index      
-      i = 0
-      while i < self.length
-        yield(i,self[i])
-        i += 1
-      end
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield(i, self[i])
+      i += 1
     end
+  end
 
   def my_select
-      result=[]
-      self.my_each{ |element| result << element if yield(element)}
-  end  
+    result = []
+    my_each{ |element| result << element if yield(element)}
+  end
 
   def my_all
-      flag=true
-      self.my_each{ |element| flag = flag && yield(element)}
-      flag
-  end  
+    flag=true
+    my_each{ |element| flag = flag && yield(element)}
+    flag
+  end
 
   def my_any
     flag = false
-    self.my_each{ |element| flag = flag || yield(element)}
+    my_each{ |element| flag = flag || yield(element)}
     flag
-  end  
+  end
 
   def my_none
-      flag = false
-      self.my_each{ |element| flag = flag || yield(element)}
-      !flag
-  end  
+    flag = false
+    self.my_each { |element| flag = flag || yield(element) }
+    !flag
+  end
 
   def my_count
-    count = 0   
-    self.my_each{ |element| count += 1 if yield(element)}
+    count = 0
+    my_each { |element| count += 1 if yield(element) }
     count
   end  
 
   def my_map(proc = nil)
-    result=[]
-    self.my_each{ |element|
+    result = []
+    my_each do |element|
       if proc
         result <<  proc.call(element)         
       else
         result << yield(element)
       end
-    }
+    end
     result
   end  
 
@@ -66,9 +66,13 @@ end
 
 
 
+  arr=[1,2,3,4]
+  arr.my_each{|element| puts element}
+  arr.my_each_with_index{|index,element| puts "index: #{index} for #{element}"}
+
 
 def multiply_els(arr)
-    arr.my_inject(1){|res,ele| res*ele}
+  arr.my_inject(1){|res,ele| res*ele}
 end
 
 puts multiply_els([2,4,5])
